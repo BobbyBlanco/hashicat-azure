156a9b03c3ca23e8894c1d560e61a9aaddeeab6d
Rob White <white255@hotmail.com>
2023-05-02 19:28:59 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,12 +1,8 @@
 //--------------------------------------------------------------------
-// Variables
-variable "resource_group_name" {}
-
-//--------------------------------------------------------------------
 // Modules
 module "network" {
   source  = "app.terraform.io/robs-testarea/network/azurerm"
   version = "3.5.0"
 
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "Gaurav"
 }
